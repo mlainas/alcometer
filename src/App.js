1412a9164179bb2f3fe9caee4d462a4b25bae2c0
Mikko Lainas <n0lami00@students.oamk.fi>
2021-09-16 14:26:21 +0300

    Valmis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import FillOptions from './FillOptions';
 
 function App() {
+  const [weight, setWeight] = useState(0);
+  const [bottles, setBottles] = useState(0);
+  const [time, setTime] = useState(0);
+  const [gender, setGender] = useState('male');
+  const [result, setResult] = useState(0);
+
+
+  function calculate(e) {
+    e.preventDefault();
+    const litres = bottles * 0.33;
+    const grams = litres * 8 * 4.5;
+    const burning = weight / 10;
+    const gramsLeft = grams - (burning * time);
+    
+    let tulos = 0;
+    if (gender === 'male') {
+      tulos = (gramsLeft / (weight * 0.7));
+    } else {
+      tulos = (gramsLeft / (weight * 0.6));
+    }
+    setResult(tulos);
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <h3>Calculating alcohol blood level</h3>
+      <form onSubmit={calculate}>
+        <div>
+          <label>Weight </label>
+          <input type="number"
+            value={weight}
+            onChange={e => setWeight(e.target.value)}/>
+        </div>
+        <div>
+          <label>Bottles (0.33l) </label>
+          <select type="number"
+            value={bottles}
+            onChange={e => setBottles(e.target.value)}>
+            <FillOptions />
+          </select>
+        </div>
+        <div>
+          <label>Time (hour) </label>
+
+          <select type="number"
+            value={time}
+            onChange={e => setTime(e.target.value)}>
+            <FillOptions />
+          </select>
+        </div>
+
+        <div>
+          <label>Gender</label>
+          <input type="radio" name="gender" value="male"
+            defaultChecked onChange={e => setGender(e.target.value)} />
+          <label>Male</label>
+
+          <input type="radio" name="gender" value="female" 
+            onChange={e => setGender(e.target.value)} />
+          <label>Female</label>
+        </div>
+
+        <div>
+          <output>{result.toFixed(2)}</output>
+
+          <button >Calculate</button>
+        </div>
+      </form>
     </div>
   );
 }
